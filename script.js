80ff722b9ebe75ebf93b77eb19cd648dbc174035
Ansh Kumar <anshkumar9686@gmail.com>
2024-11-01 16:28:54 +0530

    js

---
+++ script.js
@@ -0,0 +1,18 @@
+let compliments =[];
+
+fetch('compliments.json')
+    .then(response => response.json())
+    .then(data => {
+        compliments=data.compliments;
+    })
+    .catch(error => console.error('Error loading compliments:',error))
+
+function generateCompliment(){
+    if(compliments.length>0){
+        const randomIndex=Math.floor(Math.random()*compliments.length);
+        const compliment = compliments[randomIndex];
+        document.getElementById("compliment").textContent=compliment;
+    } else{
+        document.getElementById("compliment").textContent="Loading compliments"
+    }
+}
